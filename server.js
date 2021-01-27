bcce9e3c20f5086a6925484606c701a294ee52fa
Matheus Passos <msena789@gmail.com>
2021-01-26 22:01:48 -0300

    project structure

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.render('index'));
